c098ffe54fa91fc8e525c898239981c401a26aa5
Joseph <indiekaj@gmail.com>
2024-10-08 23:35:16 +0300

    added styling to the pages

--- emobilis_interview/settings.py
+++ emobilis_interview/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = 'static/'
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
